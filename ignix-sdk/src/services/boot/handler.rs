bdbfa9fd294ae3a5c9f7ca6d0df980a486e16180
Flamitsu <flamitsu@proton.me>
2026-07-17 17:43:13 +0200

    (sdk) feat: handler.rs and image.rs secure wrappers now has all the functions (boilerplate) I need to complete them

--- ignix-sdk/src/services/boot/handler.rs
+++ ignix-sdk/src/services/boot/handler.rs
@@ -1,3 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use crate::table::boot::BootServicesWrapper;
-impl BootServicesWrapper {}
+impl BootServicesWrapper {
+    pub fn install_protocol_interface(&self) {}
+    pub fn uninstall_protocol_interface(&self) {}
+    pub fn reinstall_protocol_interface(&self) {}
+    pub fn register_protocol_notify(&self) {}
+    pub fn locate_handle(&self) {}
+    pub fn handle_protocol(&self) {}
+    pub fn locate_device_path(&self) {}
+    pub fn open_protocol(&self) {}
+    pub fn close_protocol(self) {}
+    pub fn open_protocol_information(&self) {}
+    pub fn connect_controller(&self) {}
+    pub fn disconnect_controller(&self) {}
+    pub fn protocols_per_handle(&self) {}
+    pub fn locate_handle_buffer(&self) {}
+    pub fn locate_protocol(&self) {}
+}
--- ignix-sdk/src/services/boot/image.rs
+++ ignix-sdk/src/services/boot/image.rs
@@ -1,3 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 use crate::table::boot::BootServicesWrapper;
-impl BootServicesWrapper {}
+impl BootServicesWrapper {
+    pub fn load_image(&self) {}
+    pub fn start_image(&self) {}
+    pub fn unload_image(&self) {}
+    pub fn exit(&self) {}
+    pub fn exit_boot_services(&self) {}
+}
