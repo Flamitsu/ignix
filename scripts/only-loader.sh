abe8e72f17e130785e6048e4cec74eec8f2e0698
Flamitsu <flamitsu@proton.me>
2026-07-18 11:07:50 +0200

    fix: script fix for the new routes of edk2 tooling in Arch Linux (it worked in Gentoo)

--- scripts/only-loader.sh
+++ scripts/only-loader.sh
@@ -1,31 +1,45 @@
 #!/usr/bin/bash
-# This script will boot up a virtual machine with the structure needed for the .EFI to work. (only .EFI)
 BOOT_DIR="../boot-test"
-DEPENDENCIES_DIR="/usr/share/edk2-ovmf"
-# If the directory does not exists then it will try to setup it.
-if [ ! -d "$BOOT_DIR" ]; then
-    mkdir "$BOOT_DIR"
-    cp "$DEPENDENCIES_DIR/OVMF_CODE.fd" "$BOOT_DIR/" || { echo "Are edk2-ovmf tools installed ? OVMF_CODE.fd not found in '$DEPENDENCIES_DIR'"; exit 1; }
-    cp "$DEPENDENCIES_DIR/OVMF_VARS.fd" "$BOOT_DIR/" || { echo "Are edk2-ovmf tools installed ? OVMF_VARS.fd not found in '$DEPENDENCIES_DIR'"; exit 1; }
-    mkdir -p "$BOOT_DIR/esp/efi/boot"
-fi 
-# Will try to build the binary
-cargo core || { echo "You may need to run the 'install-targets.sh' script in this directory."; exit 1; }
-# Will try to copy the recently compiled binary
+mkdir -p "$BOOT_DIR/esp/efi/boot"
+if [ ! -f "$BOOT_DIR/OVMF_CODE.fd" ]; then
+    POSSIBLE_DIRS=(
+        "/usr/share/edk2-ovmf"
+        "/usr/share/edk2/x64"
+        "/usr/share/ovmf/x64"
+        "/usr/share/OVMF"
+    )
+    FOUND_CODE=""
+    FOUND_VARS=""
+    for dir in "${POSSIBLE_DIRS[@]}"; do
+        for suffix in "" ".4m" ".2m"; do
+            if [ -f "$dir/OVMF_CODE${suffix}.fd" ] && [ -f "$dir/OVMF_VARS${suffix}.fd" ]; then
+                FOUND_CODE="$dir/OVMF_CODE${suffix}.fd"
+                FOUND_VARS="$dir/OVMF_VARS${suffix}.fd"
+                break 2
+            fi
+        done
+    done
+    if [ -n "$FOUND_CODE" ]; then
+        cp "$FOUND_CODE" "$BOOT_DIR/OVMF_CODE.fd"
+        cp "$FOUND_VARS" "$BOOT_DIR/OVMF_VARS.fd"
+    else
+        echo "Error: The script didn't found OVMF_CODE or OVMF_VARS in your system."
+        echo "Please, ensure that you have installed the edk2-ovmf package of your distribution."
+        exit 1
+    fi
+fi
+cargo core || { echo "Error while compiling"; exit 1; }
 cp ../target/x86_64-unknown-uefi/debug/ignixx64.efi "$BOOT_DIR/esp/efi/boot/bootx64.efi"
-
 DEBUG_FLAGS=""
 if [ "$1" == "--debug" ]; then
     echo "QEMU waiting LLDB in port 1234..."
     DEBUG_FLAGS="-s -S"
 fi
-
-# Will try to run the script. 
 (
-    cd ../boot-test || { echo "Could not change to the $BOOT_DIR directory."; exit 1; }
+    cd ../boot-test || { echo "Couldn't change directories"; exit 1; }
     qemu-system-x86_64 -enable-kvm \
         -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
-        -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
+        -drive if=pflash,format=raw,readonly=off,file=OVMF_VARS.fd \
         -drive format=raw,file=fat:rw:esp \
         $DEBUG_FLAGS || { echo ""; exit 1; }
 )
