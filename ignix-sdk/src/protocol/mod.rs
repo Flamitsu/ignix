3a74e597da7d277793275b3b9d6aa6dac929c780
Flamitsu <flamitsu@proton.me>
2026-07-30 19:11:45 +0200

    (sdk) del: loadfile2protocol and the struct types in types/misc.rs

--- ignix-sdk/src/protocol/mod.rs
+++ ignix-sdk/src/protocol/mod.rs
@@ -3,8 +3,6 @@ pub mod console;
 #[allow(unused)]
 pub mod file_protocol;
 #[allow(unused)]
-pub mod load_file_2;
-#[allow(unused)]
 pub mod loaded_image;
 #[allow(unused)]
 pub mod simple_file_system_protocol;
